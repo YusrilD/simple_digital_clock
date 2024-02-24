93195cae88da2cbff48ddba152e0c9a861ef94f7
YusrilD <yusrildewantara2@gmail.com>
2024-02-24 08:39:46 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:slide_digital_clock/slide_digital_clock.dart';
+import 'package:wakelock/wakelock.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    // Start full screen
+    Wakelock.enable();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: FittedBox(
+              fit: BoxFit.scaleDown,
+              child: DigitalClock(
+                hourMinuteDigitTextStyle: GoogleFonts.orbitron(
+                  fontSize: 120,
+                  color: Colors.white,
+                ),
+                showSecondsDigit: false,
+                colon: Text(
+                  ":",
+                  style: GoogleFonts.orbitron(
+                    fontSize: 120,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+              // Text(
+              //   "08:25:25 AM",
+              //   style: GoogleFonts.orbitron(
+              //     fontSize: 60,
+              //     color: Colors.white,
+              //   ),
+              // ),
+              ),
+        ),
+      ),
+    );
+  }
+}
